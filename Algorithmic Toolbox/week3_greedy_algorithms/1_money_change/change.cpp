3dac7f49a61e1eddcfb7fe57056e62014d744c3b
Ashish Chawda <ashish11chawda@gmail.com>
2020-06-03 20:54:00 +0530

    week3 assignment 1 & 2

--- Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
+++ Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
@@ -2,6 +2,24 @@
 
 int get_change(int m) {
   //write your code here
+  int n=0;
+  
+  while(m){
+  	if(m>=10){
+  		n = n + (m/10);
+  		m = m%10;
+	}
+	else if(m>=5 && m<10){
+		n = n + (m/5);
+		m = m%5;
+	}
+	else if(m>=1){
+		n = n + m;
+		m = m%1;
+	}
+	else{	
+	}
+  }
   return n;
 }
 
--- Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,13 +1,69 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+using namespace std;
 
-using std::vector;
-
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+double get_optimal_value(int n,int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
-
-  // write your code here
-
+  int sum = 0;
+  double vbyw[n];
+  for(int i=0;i<n;i++){
+  	vbyw[i] = ((double)values[i])/((double)weights[i]);
+  }
+  /*3 50
+    v   | w
+	60  | 20
+	100 | 50
+	120 | 30
+  */
+  // vbyw ={3,2,4}
+  
+  double max= vbyw[0];
+  int index =0 ;
+ 
+  while(capacity){//iter1 capacity=50
+  //iter2  capacity=20
+  	for(int i=0;i<n;i++){
+	  if(max < vbyw[i]){
+	  	max= vbyw[i];
+	  	index=i;
+	  }	
+	}
+	//iter 1
+	// max = 4
+	// index = 2
+	//iter 2
+	// max = 3
+	// index = 0
+	
+	vbyw[index]=0.0;
+	//iter1 vbyw ={3,2,0}
+	//iter2 vbyw ={0,2,0}
+	if(weights[index]>capacity){//iter1 30<50 "x"
+	//iter2 20<20 "x"
+		int diff = weights[index]-capacity;
+		sum = sum + weights[index]-diff;
+		value= value + (sum * max);
+		capacity = capacity - sum ;
+	}
+	else if(weights[index] <= capacity){//iter1 30 < 50
+	//iter2 20==20
+		sum = sum + weights[index];
+		//iter1 0=0+30 sum=30
+		//iter2 30=30+20 sum=50
+		value= value + (sum * max);
+		//iter1 0.0 = 0.0 + (30*4) value = 120
+		//iter2 120 = 120 + (50 * 3) value = 180
+		capacity = capacity - sum ;
+		//50=50-30 capacity=20
+	}
+	else{
+		
+	}
+	sum=0;
+	max=0;
+	index=0;
+  }
   return value;
 }
 
@@ -21,7 +77,7 @@ int main() {
     std::cin >> values[i] >> weights[i];
   }
 
-  double optimal_value = get_optimal_value(capacity, weights, values);
+  double optimal_value = get_optimal_value(n,capacity, weights, values);
 
   std::cout.precision(10);
   std::cout << optimal_value << std::endl;
